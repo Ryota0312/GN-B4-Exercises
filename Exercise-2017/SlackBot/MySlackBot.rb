7358bde7d5991af527844dfb1f33409520dc9d9e
Nishi <puq77f8e@s.okayama-u.ac.jp>
2017-04-10 19:00:19 +0900

    Update MySlackBot.rb

--- Exercise-2017/SlackBot/MySlackBot.rb
+++ Exercise-2017/SlackBot/MySlackBot.rb
@@ -143,14 +143,14 @@ class GoogleStaticMaps
   end
 end
 
-class MySlackBot < SlackBot 
-  def test_respond(params, options={})
+class SlackRespond
+  def repeat_respond(params, options={})
     user_name = params[:user_name] ? "@#{params[:user_name]}" : ""
     msg=params[:text]
     msg=msg.match(/[^「]*「(.*)」と言って/)
     return {text: "#{user_name} #{msg[1]}"}.merge(options).to_json
   end
-
+  
   def respond_places_useplaceid(params, options={})
     geocode=GoogleGeocode.new
     places=GooglePlaces.new
@@ -160,7 +160,7 @@ class MySlackBot < SlackBot
     text = params[:text]
     text = text.match(/@NBot\s+(.*)/)
     input_text = text[1]
-
+    
     place_id = places.get_placeid(input_text)
     if place_id==nil
       return {text: "ERROR:地点が特定できませんでした．(#{input_text})\n"}.merge(options).to_json
@@ -179,22 +179,22 @@ class MySlackBot < SlackBot
     if places==nil
       return {text: "ERROR:結果が見つかりませんでした．\n"}.merge(options).to_json
     end
-
+    
     msg = Array.new(3)
     for i in 0..2 do
       msg[i] = "#{places[i]["name"]} : #{places[i]["address"]}\n"
     end
-
+    
     map = maps.create_map(location, places) 
-
+    
     return {text: "最寄りのコンビニ3件は以下の通りです．\nA:#{msg[0]}\nB:#{msg[1]}\nC:#{msg[2]}\n#{map}"}.merge(options).to_json
   end
-
-   def respond_places(params, options={})
+  
+  def respond_places(params, options={})
     geocode=GoogleGeocode.new
     places=GooglePlaces.new
     maps=GoogleStaticMaps.new
-
+    
     place_table = Hash[ "コンビニ" => 'convenience_store',
                         "書店" => 'book_store',
                         "ATM" => 'atm',
@@ -212,9 +212,9 @@ class MySlackBot < SlackBot
     text = text.match(/@NBot\s+(.*)(付近|近く)の(.*)/)
     address = text[1]
     placetype = text[3]
-
+    
     types = place_table["#{placetype}"]
-
+    
     location = geocode.get_location_byaddress(address)
     if location==nil
       return {text: "#{user_name}\n地点が特定できませんでした．(#{address})\n"}.merge(options).to_json
@@ -227,15 +227,30 @@ class MySlackBot < SlackBot
     if places==nil
       return {text: "#{user_name}\n結果が見つかりませんでした．\n"}.merge(options).to_json
     end
-
+    
     msg = Array.new(3)
     for i in 0..2 do
       msg[i] = "#{places[i]["name"]} : #{places[i]["address"]}\n"
     end
-
+    
     map = maps.create_map(location, places) 
+    
+    return {text: "#{user_name}\n最寄りの#{placetype}3件は以下の通りです．\nA:#{msg[0]}\nB:#{msg[1]}\nC:#{msg[2]}\n#{map}"}.merge(options).to_json
+   end
+end
 
-    return {text: "#{user_name}\n#{address}から最寄りの#{placetype}3件は以下の通りです．\nA:#{msg[0]}\nB:#{msg[1]}\nC:#{msg[2]}\n#{map}"}.merge(options).to_json
+class MySlackBot < SlackBot 
+  def bot_respond(params, options={})
+    bot = SlackRespond.new
+    user_name = params[:user_name] ? "@#{params[:user_name]}" : ""
+    input = params[:text]
+    if input =~ /[^「]*「(.*)」と言って/
+      bot.repeat_respond(params, options)
+    elsif input =~ /@NBot\s+(.*)(付近|近く)の(.*)/
+      bot.respond_places(params, options)
+    else
+      return {text: "#{user_name}\nExample:\"〇〇付近の〇〇\",\ \"「〇〇」と言って\"\n"}.merge(options).to_json
+    end
   end
 end
 
@@ -249,6 +264,6 @@ end
 
 post '/slack' do
   content_type :json
-  slackbot.respond_places(params, {username: "NBot", link_names: true})
+  #slackbot.respond_places(params, {username: "NBot", link_names: true})
+  slackbot.bot_respond(params, {username: "NBot", link_names: true})
 end
-
